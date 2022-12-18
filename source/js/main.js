8eba05b3adbc5e21db9e613b185743072dd79005
Igor Rebrishchev <igorr28@yandex.ru>
2022-12-18 22:25:08 +0300

    добавляет отложенную загрузку карт и предзагрузку фоновых изображений для мобильных

--- source/js/main.js
+++ source/js/main.js
@@ -18,9 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
   initMainMenu();
   initFormValidation();
-  // eslint-disable-next-line no-undef
-  ymaps.ready(initMap);
   scrollTo();
+  initMap();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
